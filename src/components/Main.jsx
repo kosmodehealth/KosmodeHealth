d82daf13d78d2ce904f62ccf10492d7ddcf42529
doux124 <onezeroten124@gmail.com>
2025-03-15 20:58:24 +0800

    v0 inital commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Main = () => {
+  return (
+    <div className="h-screen w-screen overflow-hidden bg-gray-900 flex flex-col">
+      Hello
+    </div>
+  );
+};
+
+export default Main;
